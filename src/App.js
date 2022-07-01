5259ac2d5101257396381f6c54fbe95753a0dc49
chrisngnstpls <chrisngnstpls@gmail.com>
2022-07-01 16:18:10 +0300

    first frontend commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-
+import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
 import getWeb3 from './ethereum/web3';
 import BetFactory from './build/BetFactory.json'
 import Bet from './build/Bet.json'
-import BetForm from './Components/BetForm';
+
 import Web3 from 'web3';
 import Layout from './Components/Layout';
 
@@ -14,10 +14,15 @@ import Layout from './Components/Layout';
 
 class App extends Component {
   state = {
+    keyMaster:0x0000000000000000000000000000000000000000,
     myVal : 0,
     web3:null,
     accounts:null,
-    factoryInstance:null
+    factoryInstance:null,
+    reason:'',
+    fee:0,
+    gas:2500000,
+    betValue:0
   }
   componentDidMount = async() => {
     try {
@@ -35,15 +40,52 @@ class App extends Component {
       throw(err);
     }
   };
-  
+  onSubmit = async (event) =>{
+
+    event.preventDefault();
+    // this does not work make sure to convert value to wei gwei or what have you *** error 32603
+    const{accounts,gas, keyMaster, factoryInstance, fee, reason, betValue} = this.state
+    //let correctAddress =await Web3.utils.toChecksumAddress(keyMaster);
+    await factoryInstance.methods.createBet(reason, keyMaster, fee).send({from:accounts[0], gas:3000000, value:betValue})
+
+  }
   render() {
-    console.log(this.state.web3)
     if(!this.state.web3){
       return <div>Loading web3 and components</div>
     } else if(this.state.web3) {
       return (
         <Layout>
-          <BetForm/>
+          <form onSubmit={this.onSubmit}>
+          <Input
+            label='Keymaster address'
+            type='text'
+            value={this.state.keyMaster}
+            maxLength='42'
+            onChange={event => this.setState({keyMaster : event.target.value})}
+          />
+          <Input 
+            label = 'Reason'
+            type='text'
+            value={this.state.reason}
+            maxLength="32"
+            onChange={event => this.setState({reason:event.target.value})}
+          />
+          <Input 
+            label = "fee"
+            type='number'
+            value={this.state.fee}
+            onChange={event => this.setState({fee:event.target.value})}
+          />
+          <Input 
+            label='Bet size'
+            type='number'
+            value={this.state.betValue}
+            onChange={event => this.setState({betValue:event.target.value})}
+          />
+
+          <Button color='red'>Start Bet</Button>
+          
+          </form>
         </Layout>
       );
     }
